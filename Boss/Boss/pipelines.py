d0692a141516e1386b7a9f48eef05dbbfb63b3da
wenying <2515957145@qq.com>
2019-04-03 11:19:32 +0800

    管道文件

--- Boss/Boss/pipelines.py
+++ Boss/Boss/pipelines.py
@@ -4,8 +4,27 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
+from Boss.settings import mongo_host, mongo_port, mongo_db_name, mongo_db_connection
 
 
 class BossPipeline(object):
+
+    def __init__(self):
+        host = mongo_host
+        port = mongo_port
+        dbname = mongo_db_name
+        tablename = mongo_db_connection
+        client = pymongo.MongoClient(host=host, port=port)
+        my_database = client[dbname]
+        self.post = my_database[tablename]
+
     def process_item(self, item, spider):
+        """
+        :param item: boss_spider中yield过来的boss_item
+        :param spider:
+        :return:
+        """
+        data = dict(item)
+        self.post.insert(data)
         return item
